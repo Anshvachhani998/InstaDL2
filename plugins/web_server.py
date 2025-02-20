e3d31362dd09a70a4dfd775fed805157e1a7a3e6
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-02-20 21:46:13 +0530

    Delete plugins/web_server.py

--- plugins/web_server.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-
-async def web_server():
-    async def handle(request):
-        return web.Response(text="Telegram Bot is Running!")
-
-    app = web.Application()
-    app.router.add_get("/", handle)
-    return app

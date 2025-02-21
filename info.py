1e65b563b36f45e27e8cdfd64b1818214ebbe517
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-02-21 13:44:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,6 +5,7 @@ API_ID = int(os.getenv("API_ID", "8012239"))
 API_HASH = os.getenv("API_HASH", "171e6f1bf66ed8dcc5140fbe827b6b08")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7277194738:AAHrewQsvKcPqeXYeMIbSk-nyUjgJ14kW8U")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002379643238"))
+DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002379643238"))
 PORT = int(os.getenv("PORT", "8080"))
 INSTAGRAM_USERNAME = "harshvi_039"
 INSTAGRAM_PASSWORD = "Ansh123@123"

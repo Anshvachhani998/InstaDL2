df4b561a687c5b41f6cf9ac793eaef9a833caa31
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-02-22 01:11:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,26 @@ from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, Peer
 from database.db import db
 
 
+from pyrogram import Client
+
+async def get_invite_link(client: Client, channel_id: str):
+    """Private channel ka invite link generate karega"""
+    try:
+        return await client.export_chat_invite_link(channel_id)
+    except Exception as e:
+        print(f"Error generating invite link: {e}")
+        return None
+
+async def is_subscribed(client: Client, user_id: int, channel_id: str):
+    """Check if user is subscribed to the force join channel"""
+    try:
+        member = await client.get_chat_member(channel_id, user_id)
+        return member.status in ["member", "administrator", "creator"]
+    except Exception:
+        return False  # If error, assume not subscribed
+
+
+
 async def broadcast_messages(user_id, message):
     try:
         await message.copy(chat_id=user_id)

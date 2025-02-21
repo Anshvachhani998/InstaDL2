a0588de11a9efeaba0782e2168004e573a216fae
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-02-21 23:21:43 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -9,17 +9,14 @@ class Database:
         self.db = self.client[MONGO_NAME]
         self.col = self.db["users"]
 
-   
+   def new_user(self, id, name):        
+        return {"id": int(id), "name": name}
+       
     async def add_user(self, id, name):
-        """Add user to database if not exists"""
-        if not await self.is_user_exist(id):
-            user = self.new_user(id, name)
-            await self.col.insert_one(user)
-            return True
-        return False
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
 
-    async def is_user_exist(self, id):
-        """Check if user already exists"""
+    async def is_user_exist(self, id):        
         user = await self.col.find_one({"id": int(id)})
         return bool(user)
 

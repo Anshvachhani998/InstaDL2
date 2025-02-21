572e0c25ab9b051c0f7c25aaaf73c7c6376cdda1
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-02-21 13:23:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ import os
 SESSION = "my_bot"
 API_ID = int(os.getenv("API_ID", "8012239"))
 API_HASH = os.getenv("API_HASH", "171e6f1bf66ed8dcc5140fbe827b6b08")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6346317908:AAH2nKFgF-MQOOzI_S30PZKNZK_aCoUhDC4")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7277194738:AAHrewQsvKcPqeXYeMIbSk-nyUjgJ14kW8U")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002379643238"))
 PORT = int(os.getenv("PORT", "8080"))
 INSTAGRAM_USERNAME = "harshvi_039"

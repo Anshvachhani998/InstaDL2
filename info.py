d60d8d82fafa80c5f522fa478380bbc127be91a4
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-05-09 20:23:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,11 +3,11 @@ import os
 SESSION = "my_bot"
 API_ID = int(os.getenv("API_ID", "8012239"))
 API_HASH = os.getenv("API_HASH", "171e6f1bf66ed8dcc5140fbe827b6b08")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7989789373:AAHLCjZfrXaKv7kgmYAAwGzWlmqx4xgtk8c")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7277194738:AAHapce3wJ-kSG3WDYG5oxg9wYxaenYwZ-8")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002238805591"))
 FORCE_CHANNEL = int(os.getenv("FORCE_CHANNEL", "-1002444509304"))
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002379643238"))
 PORT = int(os.getenv("PORT", "8080"))
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Ansh089:Ansh089@cluster0.y8tpouc.mongodb.net/?retryWrites=true&w=majority")
-MONGO_NAME = os.getenv("MONGO_NAME", "InstaDL")
+MONGO_NAME = os.getenv("MONGO_NAME", "InstaDL1")
 ADMINS = [5660839376, 6167872503]
